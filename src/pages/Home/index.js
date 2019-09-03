758b1c81b6194ee1d39792d51e3e3acd335774e6
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-09-03 11:40:05 -0300

    Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/26/D12-2759-026/D12-2759-026_zoom1.jpg"
+          alt="shoes"
+        />
+        <strong>Tênis do tênis</strong>
+        <span>R$199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={36} color="#fff" /> 12
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/26/D12-2759-026/D12-2759-026_zoom1.jpg"
+          alt="shoes"
+        />
+        <strong>Tênis do tênis</strong>
+        <span>R$199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={36} color="#fff" /> 12
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/26/D12-2759-026/D12-2759-026_zoom1.jpg"
+          alt="shoes"
+        />
+        <strong>Tênis do tênis</strong>
+        <span>R$199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={36} color="#fff" /> 12
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/26/D12-2759-026/D12-2759-026_zoom1.jpg"
+          alt="shoes"
+        />
+        <strong>Tênis do tênis</strong>
+        <span>R$199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={36} color="#fff" /> 12
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/26/D12-2759-026/D12-2759-026_zoom1.jpg"
+          alt="shoes"
+        />
+        <strong>Tênis do tênis</strong>
+        <span>R$199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={36} color="#fff" /> 12
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/26/D12-2759-026/D12-2759-026_zoom1.jpg"
+          alt="shoes"
+        />
+        <strong>Tênis do tênis</strong>
+        <span>R$199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={36} color="#fff" /> 12
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
